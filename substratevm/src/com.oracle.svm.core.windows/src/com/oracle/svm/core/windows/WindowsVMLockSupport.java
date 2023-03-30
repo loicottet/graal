9c8d032fc27d18a3886c94e78571096c02291fa5
Christian Haeubl <christian.haeubl@oracle.com>
2023-03-30 17:24:55 +0200

    Remove the dedicated assertion methods from VMMutex.
    (cherry picked from commit 74f3a5c03383eb3840f6df923f82116f33b3d477)

--- substratevm/src/com.oracle.svm.core.windows/src/com/oracle/svm/core/windows/WindowsVMLockSupport.java
+++ substratevm/src/com.oracle.svm.core.windows/src/com/oracle/svm/core/windows/WindowsVMLockSupport.java
@@ -236,7 +236,7 @@ final class WindowsVMMutex extends VMMutex {
 
     @Override
     public VMMutex lock() {
-        assertNotOwner("Recursive locking is not supported");
+        assert !isOwner() : "Recursive locking is not supported";
         Process.EnterCriticalSection(getStructPointer());
         setOwnerToCurrentThread();
         return this;
@@ -245,7 +245,7 @@ final class WindowsVMMutex extends VMMutex {
     @Override
     @Uninterruptible(reason = "Whole critical section needs to be uninterruptible.", callerMustBe = true)
     public void lockNoTransition() {
-        assertNotOwner("Recursive locking is not supported");
+        assert !isOwner() : "Recursive locking is not supported";
         Process.NoTransitions.EnterCriticalSection(getStructPointer());
         setOwnerToCurrentThread();
     }
