d328821f9e977b9e959d0fae80d65f2ab2d0b403
Soufiane Elhagani <soufiane.elhagani@oracle.com>
2024-11-20 07:54:46 +0000

    [GR-59079] Backport to 23.0: Broken for await of in the body of a module.
    PullRequest: js/3299

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "a241ca71deea0dd18c5a0364b149673160322342",
+                "version": "cbf25d49cf7e8305d0db689c8de36c2face77862",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "a241ca71deea0dd18c5a0364b149673160322342",
+                "version": "cbf25d49cf7e8305d0db689c8de36c2face77862",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
