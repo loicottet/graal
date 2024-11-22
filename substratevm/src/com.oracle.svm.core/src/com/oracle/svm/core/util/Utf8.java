09d7e8b4383b9f8ecab947ac08c5d605af9a9860
Anas El korchi <anas.el.korchi@oracle.com>
2024-11-22 15:06:12 +0100

    Adapt fix

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/util/Utf8.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/util/Utf8.java
@@ -39,8 +39,9 @@ public final class Utf8 {
     private Utf8() {
     }
 
-    @BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-24+16/src/hotspot/share/utilities/utf8.cpp#L479-L488")
     private static int utf8Size(char c) {
+        // Based On
+        // https://github.com/openjdk/jdk/blob/jdk-24+16/src/hotspot/share/utilities/utf8.cpp#L479-L488
         if ((0x0001 <= c) && (c <= 0x007F)) {
             // ASCII character
             return 1;
@@ -65,8 +66,9 @@ public final class Utf8 {
      * @return the length as {@code int} in bytes of the UTF8 representation of the string. Might
      *         return a truncated size if the value does not fit into {@code int} (see JDK-8328877).
      */
-    @BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-24+16/src/hotspot/share/utilities/utf8.cpp#L511-L526")
     public static int utf8Length(String s, int beginIndex, int endIndex) {
+        // Based On
+        // https://github.com/openjdk/jdk/blob/jdk-24+16/src/hotspot/share/utilities/utf8.cpp#L511-L526
         if (beginIndex < 0 || endIndex > s.length() || beginIndex > endIndex) {
             throw new StringIndexOutOfBoundsException();
         }
