5d3387630b89a4e4c4d775bde9e604b51420ad50
Christian Haeubl <christian.haeubl@oracle.com>
2024-12-16 11:02:45 +0100

    Fix registration of VM diagnostics.
    (cherry picked from commit 57e04154015e67e94154fb0593ce88b3984035ee)

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/SubstrateDiagnostics.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/SubstrateDiagnostics.java
@@ -27,8 +27,8 @@ package com.oracle.svm.core;
 import static com.oracle.svm.core.Uninterruptible.CALLED_FROM_UNINTERRUPTIBLE_CODE;
 import static com.oracle.svm.core.option.RuntimeOptionKey.RuntimeOptionKeyFlag.RelevantForCompilationIsolates;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.graalvm.collections.EconomicMap;
 import org.graalvm.compiler.api.replacements.Fold;
@@ -56,6 +56,11 @@ import org.graalvm.word.PointerBase;
 import org.graalvm.word.UnsignedWord;
 import org.graalvm.word.WordFactory;
 
+import com.oracle.svm.core.SubstrateDiagnostics.DiagnosticThunkRegistry;
+import com.oracle.svm.core.SubstrateDiagnostics.DumpCodeCacheHistory;
+import com.oracle.svm.core.SubstrateDiagnostics.DumpDeoptStubPointer;
+import com.oracle.svm.core.SubstrateDiagnostics.DumpRecentDeoptimizations;
+import com.oracle.svm.core.SubstrateDiagnostics.DumpRuntimeCodeInfoMemory;
 import com.oracle.svm.core.c.NonmovableArrays;
 import com.oracle.svm.core.code.CodeInfo;
 import com.oracle.svm.core.code.CodeInfoAccess;
@@ -69,7 +74,9 @@ import com.oracle.svm.core.config.ConfigurationValues;
 import com.oracle.svm.core.deopt.DeoptimizationSupport;
 import com.oracle.svm.core.deopt.DeoptimizedFrame;
 import com.oracle.svm.core.deopt.Deoptimizer;
+import com.oracle.svm.core.feature.AutomaticallyRegisteredFeature;
 import com.oracle.svm.core.feature.AutomaticallyRegisteredImageSingleton;
+import com.oracle.svm.core.feature.InternalFeature;
 import com.oracle.svm.core.graal.RuntimeCompilation;
 import com.oracle.svm.core.graal.stackvalue.UnsafeStackValue;
 import com.oracle.svm.core.heap.Heap;
@@ -100,6 +107,7 @@ import com.oracle.svm.core.threadlocal.FastThreadLocalBytes;
 import com.oracle.svm.core.threadlocal.FastThreadLocalFactory;
 import com.oracle.svm.core.threadlocal.VMThreadLocalInfos;
 import com.oracle.svm.core.util.CounterSupport;
+import com.oracle.svm.core.util.ImageHeapList;
 import com.oracle.svm.core.util.TimeUtils;
 import com.oracle.svm.core.util.VMError;
 
@@ -704,7 +712,7 @@ public class SubstrateDiagnostics {
         }
     }
 
-    private static class DumpDeoptStubPointer extends DiagnosticThunk {
+    static class DumpDeoptStubPointer extends DiagnosticThunk {
         @Override
         public int maxInvocationCount() {
             return 1;
@@ -821,7 +829,7 @@ public class SubstrateDiagnostics {
         }
     }
 
-    private static class DumpCodeCacheHistory extends DiagnosticThunk {
+    static class DumpCodeCacheHistory extends DiagnosticThunk {
         @Override
         public int maxInvocationCount() {
             return 2;
@@ -835,7 +843,7 @@ public class SubstrateDiagnostics {
         }
     }
 
-    private static class DumpRuntimeCodeInfoMemory extends DiagnosticThunk {
+    static class DumpRuntimeCodeInfoMemory extends DiagnosticThunk {
         @Override
         public int maxInvocationCount() {
             return 3;
@@ -850,7 +858,7 @@ public class SubstrateDiagnostics {
         }
     }
 
-    private static class DumpRecentDeoptimizations extends DiagnosticThunk {
+    static class DumpRecentDeoptimizations extends DiagnosticThunk {
         @Override
         public int maxInvocationCount() {
             return 1;
@@ -1200,11 +1208,15 @@ public class SubstrateDiagnostics {
     }
 
     public static class DiagnosticThunkRegistry {
-        private DiagnosticThunk[] diagnosticThunks;
+        @Platforms(Platform.HOSTED_ONLY.class) //
+        final int runtimeCompilationPosition;
+
+        private final List<DiagnosticThunk> thunks = ImageHeapList.create(DiagnosticThunk.class);
         private int[] initialInvocationCount;
 
         @Fold
         public static synchronized DiagnosticThunkRegistry singleton() {
+            /* The registry is already used very early during the image build. */
             if (!ImageSingletons.contains(DiagnosticThunkRegistry.class)) {
                 ImageSingletons.add(DiagnosticThunkRegistry.class, new DiagnosticThunkRegistry());
             }
@@ -1213,7 +1225,6 @@ public class SubstrateDiagnostics {
 
         @Platforms(Platform.HOSTED_ONLY.class)
         DiagnosticThunkRegistry() {
-            ArrayList<DiagnosticThunk> thunks = new ArrayList<>();
             thunks.add(new DumpRegisters());
             thunks.add(new DumpInstructions());
             thunks.add(new DumpTopOfCurrentThreadStack());
@@ -1230,35 +1241,77 @@ public class SubstrateDiagnostics {
             if (ImageSingletons.contains(JavaMainWrapper.JavaMainSupport.class)) {
                 thunks.add(new DumpCommandLine());
             }
+<<<<<<< HEAD
             thunks.add(new DumpCounters());
             if (RuntimeCompilation.isEnabled()) {
                 thunks.add(new DumpCodeCacheHistory());
                 thunks.add(new DumpRuntimeCodeInfoMemory());
                 thunks.add(new DumpDeoptStubPointer());
                 thunks.add(new DumpRecentDeoptimizations());
+=======
+            if (CounterSupport.isEnabled()) {
+                thunks.add(new DumpCounters());
+>>>>>>> 57e04154015 (Fix registration of VM diagnostics.)
             }
 
-            this.diagnosticThunks = thunks.toArray(new DiagnosticThunk[0]);
-            this.initialInvocationCount = new int[diagnosticThunks.length];
+            resizeInitialInvocationCount();
+            this.runtimeCompilationPosition = thunks.size();
+        }
+
+        @Platforms(Platform.HOSTED_ONLY.class)
+<<<<<<< HEAD
+        public synchronized void register(DiagnosticThunk diagnosticThunk) {
+            diagnosticThunks = Arrays.copyOf(diagnosticThunks, diagnosticThunks.length + 1);
+            diagnosticThunks[diagnosticThunks.length - 1] = diagnosticThunk;
+=======
+        public synchronized void add(DiagnosticThunk thunk) {
+            thunks.add(thunk);
+            resizeInitialInvocationCount();
+        }
+>>>>>>> 57e04154015 (Fix registration of VM diagnostics.)
+
+        @Platforms(Platform.HOSTED_ONLY.class)
+        public synchronized void add(int insertPos, DiagnosticThunk... extraThunks) {
+            for (int i = 0; i < extraThunks.length; i++) {
+                thunks.add(insertPos + i, extraThunks[i]);
+            }
+            resizeInitialInvocationCount();
+        }
+
+<<<<<<< HEAD
+=======
+        @Platforms(Platform.HOSTED_ONLY.class)
+        public synchronized void addAfter(DiagnosticThunk thunk, Class<? extends DiagnosticThunk> before) {
+            int insertPos = indexOf(before) + 1;
+            assert insertPos > 0;
+            thunks.add(insertPos, thunk);
+            resizeInitialInvocationCount();
+        }
+
+        @Platforms(Platform.HOSTED_ONLY.class)
+        private void resizeInitialInvocationCount() {
+            initialInvocationCount = new int[thunks.size()];
             Arrays.fill(initialInvocationCount, 1);
         }
 
         @Platforms(Platform.HOSTED_ONLY.class)
-        public synchronized void register(DiagnosticThunk diagnosticThunk) {
-            diagnosticThunks = Arrays.copyOf(diagnosticThunks, diagnosticThunks.length + 1);
-            diagnosticThunks[diagnosticThunks.length - 1] = diagnosticThunk;
-
-            initialInvocationCount = Arrays.copyOf(initialInvocationCount, initialInvocationCount.length + 1);
-            initialInvocationCount[initialInvocationCount.length - 1] = 1;
+        private int indexOf(Class<? extends DiagnosticThunk> clazz) {
+            for (int i = 0; i < thunks.size(); i++) {
+                if (thunks.get(i).getClass() == clazz) {
+                    return i;
+                }
+            }
+            throw VMError.shouldNotReachHere("Could not find diagnostic thunk " + clazz);
         }
 
+>>>>>>> 57e04154015 (Fix registration of VM diagnostics.)
         @Fold
         int size() {
-            return diagnosticThunks.length;
+            return thunks.size();
         }
 
         DiagnosticThunk getThunk(int index) {
-            return diagnosticThunks[index];
+            return thunks.get(index);
         }
 
         int getInitialInvocationCount(int index) {
@@ -1326,6 +1379,24 @@ public class SubstrateDiagnostics {
         @Uninterruptible(reason = CALLED_FROM_UNINTERRUPTIBLE_CODE, mayBeInlined = true)
         public static boolean implicitExceptionWithoutStacktraceIsFatal() {
             return singleton().implicitExceptionWithoutStacktraceIsFatal;
+        }
+    }
+}
+
+@AutomaticallyRegisteredFeature
+class SubstrateDiagnosticsFeature implements InternalFeature {
+    /**
+     * {@link RuntimeCompilation#isEnabled()} can't be executed in the
+     * {@link DiagnosticThunkRegistry} constructor because the feature registration is not
+     * necessarily finished. So, we need to do it at a later point in time.
+     */
+    @Override
+    public void afterRegistration(AfterRegistrationAccess access) {
+        DiagnosticThunkRegistry registry = DiagnosticThunkRegistry.singleton();
+        if (RuntimeCompilation.isEnabled()) {
+            int pos = registry.runtimeCompilationPosition;
+            SubstrateDiagnostics.DiagnosticThunk[] thunks = {new DumpCodeCacheHistory(), new DumpRuntimeCodeInfoMemory(), new DumpDeoptStubPointer(), new DumpRecentDeoptimizations()};
+            registry.add(pos, thunks);
         }
     }
 }
