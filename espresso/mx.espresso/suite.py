d60baa40336910a25171003fc9fcb1279d0bc534
Andreas Woess <andreas.woess@oracle.com>
2024-12-17 03:52:37 +0100

    Upgrade ASM dependency from 9.5 to 9.7.1.
    (cherry picked from commit a55ea605af4c0ba6209e1a5ab5f90ed9c7ff6981)

--- espresso/mx.espresso/suite.py
+++ espresso/mx.espresso/suite.py
@@ -131,7 +131,7 @@ suite = {
             "dependencies": [
                 "truffle:TRUFFLE_API",
                 "truffle:TRUFFLE_NFI",
-                "truffle:TRUFFLE_ASM_9.5",
+                "truffle:TRUFFLE_ASM_9.7.1",
                 "com.oracle.truffle.espresso.jdwp",
             ],
             "requires": [
@@ -335,7 +335,7 @@ suite = {
                 "tools:TRUFFLE_PROFILER",
             ],
             "exclude": [
-                "truffle:TRUFFLE_ASM_9.5",
+                "truffle:TRUFFLE_ASM_9.7.1",
             ],
             "javaProperties": {
                 "org.graalvm.language.java.home": "<path:ESPRESSO_SUPPORT>",
