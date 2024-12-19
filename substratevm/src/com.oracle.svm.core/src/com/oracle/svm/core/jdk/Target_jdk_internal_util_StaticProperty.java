43642c173d0207cc73453be09350fc1208e886b3
Christian Haeubl <christian.haeubl@oracle.com>
2024-12-19 14:20:25 +0100

    Various fixes for SystemProperties.

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/Target_jdk_internal_util_StaticProperty.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/Target_jdk_internal_util_StaticProperty.java
@@ -24,128 +24,155 @@
  */
 package com.oracle.svm.core.jdk;
 
-import java.nio.charset.Charset;
-import java.util.function.BooleanSupplier;
-
+import com.oracle.svm.core.SubstrateUtil;
+import com.oracle.svm.core.annotate.Alias;
+import com.oracle.svm.core.annotate.Delete;
+import com.oracle.svm.core.annotate.KeepOriginal;
+import com.oracle.svm.core.annotate.RecomputeFieldValue;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
-import com.oracle.svm.core.annotate.TargetElement;
-import com.oracle.svm.core.jdk.localization.substitutions.Target_java_nio_charset_Charset;
-import com.oracle.svm.util.ReflectionUtil;
-
-import jdk.internal.util.StaticProperty;
 
 /**
  * This class provides JDK-internal access to values that are also available via system properties.
- * However, it must not return values changes by the user. We do not want to query the values during
- * VM startup, because doing that is expensive. So we perform lazy initialization by calling the
- * same methods also used to initialize the system properties.
+ * However, it must not return values changed by the user. We do not want to query the values during
+ * VM startup, because doing that is expensive. So we perform lazy initialization by accessing the
+ * corresponding system properties.
+ * <p>
+ * We {@link Substitute substitute} the whole class so that it is possible to use a custom static
+ * constructor at run-time.
+ * <p>
+ * Note for updating: use {@link Delete} for static fields that should be unreachable (e.g, because
+ * we substituted an accessor and the field is therefore unused). Use {@link Alias} for static
+ * fields that can be initialized in our custom static constructor. Use {@link Substitute} for
+ * methods that access expensive lazily initialized system properties (see
+ * {@link SystemPropertiesSupport} for a list of all lazily initialized properties). Use
+ * {@link KeepOriginal} for methods that we don't want to substitute.
  */
 @Substitute
 @TargetClass(jdk.internal.util.StaticProperty.class)
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "FieldCanBeLocal"})
 final class Target_jdk_internal_util_StaticProperty {
+    // Checkstyle: stop
+    @Delete//
+    private static String JAVA_HOME;
+
+    @Delete//
+    private static String USER_HOME;
+
+    @Delete//
+    private static String USER_DIR;
+
+    @Delete//
+    private static String USER_NAME;
+
+    @Delete//
+    private static String JAVA_LIBRARY_PATH;
+
+    @Alias//
+    @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.Reset)//
+    private static String SUN_BOOT_LIBRARY_PATH;
+
+    @Alias//
+    @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.Reset)//
+    private static String JDK_SERIAL_FILTER;
+
+    @Alias//
+    @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.Reset)//
+    private static String JDK_SERIAL_FILTER_FACTORY;
+
+    @Delete//
+    private static String JAVA_IO_TMPDIR;
+
+    @Alias//
+    @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.Reset)//
+    private static String NATIVE_ENCODING;
+
+    @Alias//
+    @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.Reset)//
+    private static String FILE_ENCODING;
+
+    @Alias//
+    @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.Reset)//
+    private static String JAVA_PROPERTIES_DATE;
+
+    @Alias//
+    @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.Reset)//
+    private static String SUN_JNU_ENCODING;
+
+    @Alias//
+    @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.Reset)//
+    private static String JAVA_LOCALE_USE_OLD_ISO_CODES;
+
+    /*
+     * This static constructor is executed at run-time. Be careful that it only initializes lazy
+     * system properties that are reasonably cheap to initialize (e.g., everything related to
+     * locale).
+     */
+    static {
+        if (!SubstrateUtil.HOSTED) {
+            SystemPropertiesSupport p = SystemPropertiesSupport.singleton();
+            SUN_BOOT_LIBRARY_PATH = p.getInitialProperty("sun.boot.library.path", "");
+            JDK_SERIAL_FILTER = p.getInitialProperty("jdk.serialFilter");
+            JDK_SERIAL_FILTER_FACTORY = p.getInitialProperty("jdk.serialFilterFactory");
+            NATIVE_ENCODING = p.getInitialProperty("native.encoding");
+            FILE_ENCODING = p.getInitialProperty("file.encoding");
+            JAVA_PROPERTIES_DATE = p.getInitialProperty("java.properties.date");
+            SUN_JNU_ENCODING = p.getInitialProperty("sun.jnu.encoding");
+            JAVA_LOCALE_USE_OLD_ISO_CODES = p.getInitialProperty("java.locale.useOldISOCodes", "");
+        }
+    }
 
     @Substitute
     private static String javaHome() {
-        /* Native images do not have a Java home directory. */
-        return null;
+        return SystemPropertiesSupport.singleton().getInitialProperty("java.home");
     }
 
     @Substitute
     private static String userHome() {
-        return SystemPropertiesSupport.singleton().userHome();
+        return SystemPropertiesSupport.singleton().getInitialProperty(UserSystemProperty.HOME);
     }
 
     @Substitute
     private static String userDir() {
-        return SystemPropertiesSupport.singleton().userDir();
+        return SystemPropertiesSupport.singleton().getInitialProperty(UserSystemProperty.DIR);
     }
 
     @Substitute
     private static String userName() {
-        return SystemPropertiesSupport.singleton().userName();
-    }
-
-    @Substitute
-    private static String javaIoTmpDir() {
-        return SystemPropertiesSupport.singleton().javaIoTmpDir();
+        return SystemPropertiesSupport.singleton().getInitialProperty(UserSystemProperty.NAME);
     }
 
     @Substitute
     private static String javaLibraryPath() {
-        return SystemPropertiesSupport.singleton().javaLibraryPath();
+        return SystemPropertiesSupport.singleton().getInitialProperty("java.library.path", "");
     }
 
     @Substitute
-    private static String sunBootLibraryPath() {
-        String value = SystemPropertiesSupport.singleton().savedProperties.get("sun.boot.library.path");
-        return value == null ? "" : value;
+    private static String javaIoTmpDir() {
+        return SystemPropertiesSupport.singleton().getInitialProperty("java.io.tmpdir");
     }
 
-    @Substitute
-    private static String jdkSerialFilter() {
-        return SystemPropertiesSupport.singleton().savedProperties.get("jdk.serialFilter");
-    }
+    @KeepOriginal
+    public static native String sunBootLibraryPath();
 
-    @Substitute
-    @TargetElement(onlyWith = StaticPropertyJdkSerialFilterFactoryAvailable.class)
-    private static String jdkSerialFilterFactory() {
-        return SystemPropertiesSupport.singleton().savedProperties.get("jdk.serialFilterFactory");
-    }
+    @KeepOriginal
+    public static native String jdkSerialFilter();
 
-    private abstract static class StaticPropertyMethodAvailable implements BooleanSupplier {
+    @KeepOriginal
+    public static native String jdkSerialFilterFactory();
 
-        private final String methodName;
+    @KeepOriginal
+    public static native String nativeEncoding();
 
-        protected StaticPropertyMethodAvailable(String methodName) {
-            this.methodName = methodName;
-        }
+    @KeepOriginal
+    public static native String fileEncoding();
 
-        @Override
-        public boolean getAsBoolean() {
-            return ReflectionUtil.lookupMethod(true, StaticProperty.class, methodName) != null;
-        }
-    }
+    @KeepOriginal
+    public static native String javaPropertiesDate();
 
-    /*
-     * Method jdkSerialFilterFactory is present in some versions of the JDK11 and not in the other.
-     * It is always present in the JDK17. We need to check if this method should be substituted by
-     * checking if it exists in the running JDK version.
-     */
-    private static class StaticPropertyJdkSerialFilterFactoryAvailable extends StaticPropertyMethodAvailable {
-        protected StaticPropertyJdkSerialFilterFactoryAvailable() {
-            super("jdkSerialFilterFactory");
-        }
-    }
+    @KeepOriginal
+    public static native String jnuEncoding();
 
-    @Substitute
-    public static String nativeEncoding() {
-        return SystemPropertiesSupport.singleton().savedProperties.get("native.encoding");
-    }
-
-    @Substitute
-    @TargetElement(onlyWith = JDK19OrLater.class)
-    public static String fileEncoding() {
-        return SystemPropertiesSupport.singleton().savedProperties.get("file.encoding");
-    }
-
-    @Substitute
-    @TargetElement(onlyWith = JDK19OrLater.class)
-    public static String javaPropertiesDate() {
-        return SystemPropertiesSupport.singleton().savedProperties.getOrDefault("java.properties.date", null);
-    }
-
-    @Substitute
-    @TargetElement(onlyWith = JDK19OrLater.class)
-    public static String jnuEncoding() {
-        return SystemPropertiesSupport.singleton().savedProperties.get("sun.jnu.encoding");
-    }
-
-    @Substitute
-    @TargetElement(onlyWith = {JDK19OrLater.class, JDK20OrEarlier.class})
-    public static Charset jnuCharset() {
-        String jnuEncoding = SystemPropertiesSupport.singleton().savedProperties.get("sun.jnu.encoding");
-        return Target_java_nio_charset_Charset.forName(jnuEncoding, Charset.defaultCharset());
-    }
+    @KeepOriginal
+    public static native String javaLocaleUseOldISOCodes();
 }
