6989a9e857baa748df9a0fff133a2d59ebcb5371
Matt D'Souza <matt.dsouza@oracle.com>
2025-01-14 14:34:30 -0500

    Fix and re-enable conditional configuration tests
    (cherry picked from commit ef177c1a6608383c20ee77037ea6007f2f763a09)

--- substratevm/src/com.oracle.svm.configure.test/src/com/oracle/svm/configure/test/conditionalconfig/ConfigurationVerifier.java
+++ substratevm/src/com.oracle.svm.configure.test/src/com/oracle/svm/configure/test/conditionalconfig/ConfigurationVerifier.java
@@ -24,6 +24,8 @@
  */
 package com.oracle.svm.configure.test.conditionalconfig;
 
+import static org.junit.Assume.assumeTrue;
+
 import java.io.IOException;
 import java.io.StringWriter;
 import java.net.URL;
@@ -34,20 +36,23 @@ import org.junit.Test;
 
 import com.oracle.svm.configure.config.ConfigurationFileCollection;
 import com.oracle.svm.configure.config.ConfigurationSet;
+import com.oracle.svm.configure.test.AddExports;
 import com.oracle.svm.core.util.json.JsonWriter;
 import com.oracle.svm.core.configure.ConfigurationFile;
 import com.oracle.svm.core.util.VMError;
 
+/**
+ * Test that validates the configuration metadata produced by the agent. This test is invoked
+ * manually from mx.
+ */
+@AddExports({"org.graalvm.nativeimage/org.graalvm.nativeimage.impl", "org.graalvm.compiler/org.graalvm.compiler.util"})
 public class ConfigurationVerifier {
 
     public static final String CONFIG_PATH_PROPERTY = ConfigurationVerifier.class.getName() + ".configpath";
 
     @Test
     public void testConfig() throws Exception {
-        String enabledProperty = System.getProperty(ConfigurationVerifier.class.getName() + ".enabled");
-        if (!Boolean.parseBoolean(enabledProperty)) {
-            return;
-        }
+        assumeTrue("Test must be explicitly enabled because it is not designed for regular execution", Boolean.getBoolean(ConfigurationVerifier.class.getName() + ".enabled"));
         ConfigurationSet actualConfig = loadActualConfig();
         ConfigurationSet expectedConfig = loadExpectedConfig();
 
