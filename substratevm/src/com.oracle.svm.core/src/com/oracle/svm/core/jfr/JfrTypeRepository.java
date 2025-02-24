489d1dd6f58170b4104e7aa4a952a048dde3b580
Robert Toyonaga <rtoyonag@redhat.com>
2025-02-24 16:54:34 -0500

    Process each class' classloader in JfrTypeRepository
    (cherry picked from commit 2a8e70d86505f834e81bbc5a6551e572b5455841)

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jfr/JfrTypeRepository.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jfr/JfrTypeRepository.java
@@ -106,6 +106,7 @@ public class JfrTypeRepository implements JfrRepository {
 
     private void visitClass(TypeInfo typeInfo, Class<?> clazz) {
         if (clazz != null && addClass(typeInfo, clazz)) {
+            visitClassLoader(typeInfo, clazz.getClassLoader());
             visitPackage(typeInfo, clazz.getPackage(), clazz.getModule());
             visitClass(typeInfo, clazz.getSuperclass());
         }
