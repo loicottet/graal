2d949601fcdde4b3f8c1c551504789748706b141
Jan Stola <jan.stola@oracle.com>
2025-02-27 15:38:51 +0000

    [GR-61884] Backport to 23.0: Upgrading the underlying Node.js to version 18.20.6.
    PullRequest: js/3404

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "caaf23c708cc25ed44628a898630674dcb1a062a",
+                "version": "7ed275b06de3d213502b7b0223d4563d23f95fb9",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "caaf23c708cc25ed44628a898630674dcb1a062a",
+                "version": "7ed275b06de3d213502b7b0223d4563d23f95fb9",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
