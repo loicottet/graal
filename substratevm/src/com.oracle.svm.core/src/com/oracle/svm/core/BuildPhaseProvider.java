13523b86c9dd96e063f816513095eb364a486ec3
aelkorch <anas.el.korchi@oracle.com>
2025-03-23 16:00:58 +0000

    Fix conflict

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/BuildPhaseProvider.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/BuildPhaseProvider.java
@@ -62,25 +62,6 @@ public final class BuildPhaseProvider {
         return ImageSingletons.contains(BuildPhaseProvider.class) && singleton().hostedUniverseBuilt;
     }
 
-<<<<<<< HEAD
-=======
-    public static void markReadyForCompilation() {
-        singleton().readyForCompilation = true;
-    }
-
-    public static boolean isReadyForCompilation() {
-        return ImageSingletons.contains(BuildPhaseProvider.class) && singleton().readyForCompilation;
-    }
-
-    public static void markCompileQueueFinished() {
-        singleton().compileQueueFinished = true;
-    }
-
-    public static boolean isCompileQueueFinished() {
-        return ImageSingletons.contains(BuildPhaseProvider.class) && singleton().compileQueueFinished;
-    }
-
->>>>>>> 7c61cfc1fe5 (Fix BuildPhaseProvider.is*Finished() so that these methods can be used at any time during the image build.)
     public static void markCompilationFinished() {
         singleton().compilationFinished = true;
     }
@@ -88,58 +69,4 @@ public final class BuildPhaseProvider {
     public static boolean isCompilationFinished() {
         return ImageSingletons.contains(BuildPhaseProvider.class) && singleton().compilationFinished;
     }
-<<<<<<< HEAD
-=======
-
-    public static void markHeapLayoutFinished() {
-        singleton().heapLayoutFinished = true;
-    }
-
-    public static boolean isHeapLayoutFinished() {
-        return ImageSingletons.contains(BuildPhaseProvider.class) && singleton().heapLayoutFinished;
-    }
-
-    public static class AfterAnalysis implements BooleanSupplier {
-        @Override
-        public boolean getAsBoolean() {
-            return BuildPhaseProvider.isAnalysisFinished();
-        }
-    }
-
-    public static class AfterHostedUniverse implements BooleanSupplier {
-        @Override
-        public boolean getAsBoolean() {
-            return BuildPhaseProvider.isHostedUniverseBuilt();
-        }
-    }
-
-    public static class ReadyForCompilation implements BooleanSupplier {
-        @Override
-        public boolean getAsBoolean() {
-            return BuildPhaseProvider.isReadyForCompilation();
-        }
-    }
-
-    public static class CompileQueueFinished implements BooleanSupplier {
-        @Override
-        public boolean getAsBoolean() {
-            return BuildPhaseProvider.isCompileQueueFinished();
-        }
-    }
-
-    public static class AfterCompilation implements BooleanSupplier {
-        @Override
-        public boolean getAsBoolean() {
-            return BuildPhaseProvider.isCompilationFinished();
-        }
-    }
-
-    public static class AfterHeapLayout implements BooleanSupplier {
-        @Override
-        public boolean getAsBoolean() {
-            return BuildPhaseProvider.isHeapLayoutFinished();
-        }
-    }
-
->>>>>>> 7c61cfc1fe5 (Fix BuildPhaseProvider.is*Finished() so that these methods can be used at any time during the image build.)
 }
