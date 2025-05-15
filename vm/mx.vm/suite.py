47d4b7b64e7ed2dce6d774e751bd46fe963b843a
Anas Korchi <anas.el.korchi@oracle.com>
2025-05-15 20:24:17 +0000

    [GR-63246] Backport to 23.1: Fix ClassCastException in super property access.
    PullRequest: js/3501

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -33,7 +33,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "8d0489980b756f5b8dd46f3b26b672f5730cb5d1",
+                "version": "6c3d2f3d70f147483809b1cd0675a61f5b0f7846",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
@@ -42,7 +42,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "8d0489980b756f5b8dd46f3b26b672f5730cb5d1",
+                "version": "6c3d2f3d70f147483809b1cd0675a61f5b0f7846",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
