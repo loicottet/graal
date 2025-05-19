5b9ad7eff61f0ff15d45145c99970b3f06bbfc93
Anas Korchi <anas.el.korchi@oracle.com>
2025-05-19 14:45:49 +0000

    [GR-63060] Backport 23.0: Map.prototype.forEach needs to import key and value of foreign hash entries.
    PullRequest: js/3503

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "863ae1ad4e6cbee2bb27fd3b9ea7c45739d45371",
+                "version": "76b075ffdcac214dc903522ea2aeeb97729dd5a6",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "863ae1ad4e6cbee2bb27fd3b9ea7c45739d45371",
+                "version": "76b075ffdcac214dc903522ea2aeeb97729dd5a6",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
