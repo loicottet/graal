e905bb179c2e497c56db63decce6dba391b11bd5
Karim Assifar <karim.assifar@oracle.com>
2025-06-18 09:26:48 +0000

    [GR-65311] Backport to 23.0: Dates with missing UTC offset should be interpreted (in nashorn-compat mode) as UTC time when they are "strict" only.
    PullRequest: js/3525

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "2132adb0e7339c15bdd2d32d19963b60a065ae50",
+                "version": "eadc56f59348165584cc39736f44d235a2ce7ff4",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "2132adb0e7339c15bdd2d32d19963b60a065ae50",
+                "version": "eadc56f59348165584cc39736f44d235a2ce7ff4",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
